871aa2b94a71bd1c50116876a21c2b94c822e65b
brelkh <brandoneli.kheng@gmail.com>
2022-07-26 19:15:51 +0800

    Initial Commit

---
+++ TotoSim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TotoSim
+//
+//  Created by Brandon Kheng on 26/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TotoSim/TotoSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotoSimApp.swift
+//  TotoSim
+//
+//  Created by Brandon Kheng on 26/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TotoSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
